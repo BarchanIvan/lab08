f8447ff2aea83c32ee603e93b5b93f8ebc3bff86
BarchanIvan <92757048+BarchanIvan@users.noreply.github.com>
2022-12-13 15:20:42 +0000

    Add files via upload

---
+++ lab08/dist/html/search/all_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['primenumber_3',['primenumber',['../main_8c.html#adc01cec8aef052e8368b6a5b58275d03',1,'main.c']]]
+];
---
+++ lab08/dist/html/search/functions_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['main_5',['main',['../main_8c.html#ae66f6b31b5ad750f1fe042a706a4e3d4',1,'main.c']]],
+  ['matrix_6',['matrix',['../main_8c.html#a563ae340e34dfd5704c6d07ce5fc7cc3',1,'main.c']]]
+];
