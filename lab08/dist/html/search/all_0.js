97854c7066f7d4bde8acc69a81683facdc504b54
BarchanIvan <92757048+BarchanIvan@users.noreply.github.com>
2022-12-13 19:25:11 +0200

    Add files via upload

---
+++ lab08/dist/html/search/all_0.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['main_0',['main',['../main_8c.html#ae66f6b31b5ad750f1fe042a706a4e3d4',1,'main.c']]],
+  ['main_2ec_1',['main.c',['../main_8c.html',1,'']]],
+  ['matrix_2',['matrix',['../main_8c.html#a563ae340e34dfd5704c6d07ce5fc7cc3',1,'main.c']]]
+];
